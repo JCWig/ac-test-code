109c0040655fcb1e5ae2adf2a7c08fafceb02b45
Leviel, Yair <yleviel@akamai.com>
2015-02-03 14:17:30 -0500

    FEE-462 Removed example code from ngdocs and created a better demo page for the indeterminate progress.

--- src/indeterminate-progress/index.js
+++ src/indeterminate-progress/index.js
@@ -19,7 +19,7 @@ var angular = require('angular');
      *
      * @description Creates an indeterminate progress control
      *
-     * @restrict E
+     * @restrict AE
      *
      * @param {String} [label=''] The label to display underneath the
      * spinner.  If omitted, the label does not display.
@@ -29,27 +29,6 @@ var angular = require('angular');
      *
      * @param {Boolean} [completed=false] Indicates if the
      * indeterminate progress encountered a `completed` state.
-     *
-     * @example This example shows the minimum configuration needed to
-     * initialize an `indeterminateProgress` component.
-     *
-     * <example module="indeterminateProgressDemo">
-     * <file name="index.html">
-     * <div ng-controller="Ctrl" class="common-css">
-     *   <div style="height: 100px;">
-     *       <akam-indeterminate-progress label="Loading your property version" completed="{{completed}}"></akam-indeterminate-progress>
-     *       The above spinner changes from started to completed state after 5 seconds.
-     *   </div>
-     * </div>
-     * </file>
-     * <file name="Ctrl.js">
-     *   var app = angular.module("indeterminateProgressDemo", ["akamai.components.indeterminate-progress"]);
-     *   app.controller("Ctrl", ["$scope", "$timeout", function($scope, $timeout) {
-     *    $scope.completed = false;
-     *    $timeout(function(){ $scope.completed = true; }, 5000);
-     *   }]);
-     * </file>
-     * </example>
      *
      */
 .directive("akamIndeterminateProgress", require('./indeterminate-progress'));
