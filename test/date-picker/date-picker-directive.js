58d68e9a14d5908dfa958f85aae23f3269aaaaab
Kevin Kotosky <kkotosky@akamai.com>
2015-02-12 13:34:29 -0500

    removed tests that are for unimplemented features, updated two edge cases

--- test/date-picker/date-picker-directive.js
+++ test/date-picker/date-picker-directive.js
@@ -57,7 +57,7 @@ describe('akam-date-picker', function() {
     });
     describe('when date picker is loaded', function(){
         beforeEach(function(){
-            var markup = '<div id="parent-element"><akam-date-picker value="picked1"></akam-date-picker></div>';
+            var markup = '<div id="parent-element"><akam-date-picker mode="day" value="picked1" onchange="mychange(value)"></akam-date-picker></div>';
             addElement(markup);
             utilities.click(document.querySelector('button.button')); 
         });
@@ -68,12 +68,13 @@ describe('akam-date-picker', function() {
         });
         it('should start on todays month', function(){
             var todaysDate = utilities.getMonthInEnglish() + " "+ utilities.getTodaysYear();
-            expect(document.querySelector('button strong.ng-binding').textContent).to.equal(todaysDate);
+            expect(document.querySelector('ul.dropdown-menu').getAttribute('style')).to.contain('display: block'); 
+            expect(document.querySelector('button.btn strong.ng-binding').textContent).to.equal(todaysDate);
         });
     });
     describe('when interacting with the date picker', function(){
         beforeEach(function(){
-            var markup = '<div id="parent-element"><akam-date-picker value="picked1" onchange="mychange(value)"></akam-date-picker></div>';
+            var markup = '<div id="parent-element"><akam-date-picker mode="day" value="picked1" onchange="mychange(value)"></akam-date-picker></div>';
             scope.mychange = sinon.spy();
             addElement(markup);
             utilities.click(document.querySelector('button.button')); 
@@ -91,13 +92,6 @@ describe('akam-date-picker', function() {
             var year = utilities.getTodaysYear();
             var next_month = month + " "+ year;
             expect(document.querySelector('button strong.ng-binding').textContent).to.equal(next_month);
-        });
-        it('should load up month screen of current year when prompted', function(){
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("day");
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("month");  
-            var year = utilities.getTodaysYear();
-            expect(document.querySelector('button strong.ng-binding').textContent).to.equal(String(year));
         });
         it('should close and save date when date is chosen', function(){
             var day = findCertainButton("01");
@@ -120,56 +114,6 @@ describe('akam-date-picker', function() {
             var firstOfThisMonth = utilities.getTodaysYear()+"-"+utilities.formatInteger(2,String(utilities.getTodaysMonth()+1))+"-02";
             expect(document.querySelector('input.ng-valid-date').value).to.equal(firstOfThisMonth);
             expect(document.querySelector('ul.dropdown-menu').getAttribute('style')).to.contain('display: none'); 
-        });
-    });
-    describe('when interacting with the month picker via date picker', function(){
-        beforeEach(function(){
-            var markup = '<div id="parent-element"><akam-date-picker value="picked1"></akam-date-picker></div>';
-            addElement(markup);
-            utilities.click(document.querySelector('button.button')); 
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-        });
-        it('should change year left when prompted', function(){
-            utilities.click(document.querySelector('button.pull-left')); 
-            var year = String(utilities.getTodaysYear()-1);
-            expect(document.querySelector('button strong.ng-binding').textContent).to.equal(year);
-        });
-        it('should change year rights when prompted', function(){
-            utilities.click(document.querySelector('button.pull-right')); 
-            var year = String(utilities.getTodaysYear()+1);
-            expect(document.querySelector('button strong.ng-binding').textContent).to.equal(year);
-        });
-        it('should load date picker of chosen month when prompted', function(){
-            utilities.click(document.querySelector('td.ng-scope button.btn-default'));
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/January/); 
-        });
-        it('should load year picker when prompted', function(){
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("month");
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("year");  
-        });
-    });
-    describe('when interacting with the year picker', function(){
-        beforeEach(function(){
-            var markup = '<div id="parent-element"><akam-date-picker value="picked1"></akam-date-picker></div>';
-            addElement(markup);
-            utilities.click(document.querySelector('button.button')); 
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-        });
-        it('should change decades right when prompted', function(){
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/2001 - 2020/);
-            utilities.click(document.querySelector('button.pull-right')); 
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/2021 - 2040/);
-        });
-        it('should change decades left when prompted', function(){
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/2001 - 2020/);
-            utilities.click(document.querySelector('button.pull-left')); 
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/1981 - 2000/);
-        });
-        it('should load month picker of chosen year when prompted', function(){
-            utilities.click(document.querySelector('td.ng-scope button.btn-default'));
-            expect(document.querySelector('button strong.ng-binding').textContent).to.match(/2001/); 
         });
     });
     describe('when rendering month picker', function(){
@@ -203,11 +147,6 @@ describe('akam-date-picker', function() {
             var next_year = String(utilities.getTodaysYear()+1);
             expect(document.querySelector('button strong.ng-binding').textContent).to.equal(next_year);
         });
-        it('should load year picker when prompted', function(){
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("month");
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            expect(document.querySelector('table').getAttribute('ng-switch-when')).to.equal("year");  
-        });
         it('should close and save month when month is chosen', function(){
             var month = findCertainButton("January");
             utilities.click(month.querySelector('button'));
@@ -231,7 +170,7 @@ describe('akam-date-picker', function() {
             expect(document.querySelector('ul.dropdown-menu').getAttribute('style')).to.contain('display: none'); 
         });
     });
-    describe('when interacting with min and max date', function(){
+    describe('when interacting with min and max date dat picker', function(){
         beforeEach(function(){
             var markup = '<div id="parent-element"><akam-date-picker min={{min}} max="{{max}}" mode="day" onchange="mychange(value)" value="picked1"></akam-date-picker></div>';
             scope.min = new Date(utilities.getTodaysYear(), utilities.getTodaysMonth(), 5);
@@ -248,42 +187,42 @@ describe('akam-date-picker', function() {
             var dayBelowMin = findCertainButton("02");
             expect(dayBelowMin.getAttribute('aria-disabled')).to.match(/true/);
         });
+        it('should be able to choose date within range', function(){
+            var dayWithinRange = findCertainButton("09");
+            expect(dayWithinRange.getAttribute('aria-disabled')).to.match(/false/);
+        });
+    });
+    describe('when interacting with min and max date month picker', function(){
+        beforeEach(function(){
+            var markup = '<div id="parent-element"><akam-date-picker min={{min}} max="{{max}}" mode="month" onchange="mychange(value)" value="picked1"></akam-date-picker></div>';
+            scope.min = new Date(utilities.getTodaysYear(), utilities.getTodaysMonth(), 5);
+            scope.max = new Date(utilities.getTodaysYear(), utilities.getTodaysMonth(), 15);
+            scope.mychange = sinon.spy();
+            addElement(markup);
+            utilities.click(document.querySelector('button.button')); 
+        });
         it('should be unable to choose month below minimum', function(){
             utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            var monthBelowMin = findCertainButton(utilities.getMonthInEnglish(utilities.getTodaysMonth()-1));
+            var monthBelowMinEnglish= utilities.getMonthInEnglish(utilities.getTodaysMonth()-1);
+            if(monthBelowMinEnglish === "December"){
+                utilities.click(document.querySelector('button.pull-left')); 
+            }
+            var monthBelowMin = findCertainButton(monthBelowMinEnglish);
             expect(monthBelowMin.getAttribute('aria-disabled')).to.match(/true/);
         });
         it('should be unable to choose month above maximum', function(){
             utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            var monthAboveMax = findCertainButton(utilities.getMonthInEnglish(utilities.getTodaysMonth()+1));
+            var monthAboveMaxEnglish = utilities.getMonthInEnglish(utilities.getTodaysMonth()+1);
+            if(monthAboveMaxEnglish === "January"){
+                utilities.click(document.querySelector('button.pull-right')); 
+            }
+            var monthAboveMax = findCertainButton(monthAboveMaxEnglish);
             expect(monthAboveMax.getAttribute('aria-disabled')).to.match(/true/);
-        });
-        it('should be unable to choose year below minimum', function(){
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            var yearBelowMin = findCertainButton(String(utilities.getTodaysYear()-1));
-            expect(yearBelowMin.getAttribute('aria-disabled')).to.match(/true/);
-        });
-        it('should be unable to choose year above maximum', function(){
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            var yearAboveMax = findCertainButton(String(utilities.getTodaysYear()+1));
-            expect(yearAboveMax.getAttribute('aria-disabled')).to.match(/true/);
-        });
-        it('should be able to choose date within range', function(){
-            var dayWithinRange = findCertainButton("09");
-            expect(dayWithinRange.getAttribute('aria-disabled')).to.match(/false/);
         });
         it('should be able to choose month within range', function(){
             utilities.click(document.querySelector('button strong.ng-binding').parentNode);
             var monthWithinRange = findCertainButton(utilities.getMonthInEnglish());
             expect(monthWithinRange.getAttribute('aria-disabled')).to.match(/false/);
-        });
-        it('should be able to choose year within range', function(){
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            utilities.click(document.querySelector('button strong.ng-binding').parentNode);
-            var yearWithinRange = findCertainButton(utilities.getTodaysYear());
-            expect(yearWithinRange.getAttribute('aria-disabled')).to.match(/false/);
         });
     });
 });
