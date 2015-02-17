742420eade513e978928a44de62f2cc70d9ce471
Mike Sierra <msierra@akamai.com>
2015-02-17 10:00:34 -0500

    minor reword & formatting

--- src/modal-window/modal-window-service.js
+++ src/modal-window/modal-window-service.js
@@ -10,50 +10,49 @@ module.exports = function($modal, $templateCache, $rootScope, $q) {
          *
          * @methodOf akamai.components.modal-window.service:modalWindow
          *
-         * @description Opens a new modal window with a backdrop with
-         * the specified options.
+         * @description Opens a new modal window.
          *
          * @param {object} options A hash with the specified options:
          *
-         *   - `template` (string) An inline template to render
-         *     within the modal body.
+         * - `template` (string) An inline template to render
+         *   within the modal body.
          *
-         *   - `templateUrl` (string) A URL referencing a template
-         *     to render within the modal body.
+         * - `templateUrl` (string) A URL referencing a template
+         *   to render within the modal body.
          *
-         *   - `controller` (Function or string) A controller for the
-         *     modal instance that can initialize scope.
+         * - `controller` (Function or string) A controller for the
+         *   modal instance that can initialize scope.
          *
-         *   - `scope` (Scope) (default: `$rootScope`) A scope
-         *     instance to use for the modal body content.
+         * - `scope` (Scope) (default: `$rootScope`) A scope
+         *   instance to use for the modal body content.
          *
-         *   - `title` (string) (default: `Modal Window Title`) A
-         *     title for the modal window.
+         * - `title` (string) (default: `Modal Window Title`) A
+         *   title for the modal window.
          *
-         *   - `icon` (string) A CSS class representing an icon to
-         *     display to the left of the modal window title.
+         * - `icon` (string) A CSS class representing an icon to
+         *   display to the left of the modal window title.
          *
-         *   - `cancelLabel` (string) (default: `Cancel`) A label
-         *     for the cancel button.
+         * - `cancelLabel` (string) (default: `Cancel`) A label
+         *   for the cancel button.
          *
-         *   - `submitLabel` (string) (default: `Save`) A label for
-         *     the submit button.
+         * - `submitLabel` (string) (default: `Save`) A label for
+         *   the submit button.
          *
-         *   - `hideSubmit` (boolean) (default: `false`) A flag to
-         *     hide the submit button and only allow the modal to be
-         *     dismissed.
+         * - `hideSubmit` (boolean) (default: `false`) A flag to
+         *   hide the submit button and only allow the modal to be
+         *   dismissed.
          *
          * @return {object} An instance of the modal with the following
          * properties:
          *
-         *   - `close` (Function) A method to close the modal window
-         *     that accepts a result as an argument.
+         * - `close` (Function) A method to close the modal window
+         *   that accepts a result as an argument.
          *
-         *   - `dismiss` (Function) A method to dismiss the modal
-         *     window, rejecting the `result` promise.
+         * - `dismiss` (Function) A method to dismiss the modal
+         *   window, rejecting the `result` promise.
          *
-         *   - `result` (Promise) A promise representing the result
-         *     when the modal window is closed.
+         * - `result` (Promise) A promise representing the result
+         *   when the modal window is closed.
          *
          */
         open: function(options) {
