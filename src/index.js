9e76f424cbea149f7926a42c4247621aa8773f37
Leviel, Yair <yleviel@akamai.com>
2015-02-19 20:28:33 -0500

    FEE-182 Initial listbox implementation.  Includes copying of original data store into a view for later sorting capability that won't affect the outside objects as well as auto detection of the column sort type based on the value of the first row.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@
 var angular = require('angular');
 
 angular.module('akamai.components', [
+    require('./id-service').name,
     require('./indeterminate-progress').name,
     require('./menu-button').name,
     require('./modal-window').name,
     require('./message-box').name,
     require('./status-message').name,
     require('./date-picker').name,
-    require('./pagination').name
+    require('./pagination').name,
+    require('./list-box').name
 ])
 
 .constant('VERSION', require('../package.json').version);
