9a0a29fef91e51c9ea0e581cc115563734effbe6
Kevin Kotosky <kkotosky@akamai.com>
2015-03-02 15:41:15 -0500

    Tests for rendering, sorting and changing pages

--- test/zdata-table/data-table-directive.js
+++ test/zdata-table/data-table-directive.js
@@ -27,6 +27,18 @@ describe('zakam-data-table', function() {
     beforeEach(function() {
         self = this;
         angular.mock.module(require('../../src/data-table').name);
+        angular.mock.module(function($provide, $translateProvider) {
+            $provide.factory('i18nCustomLoader', function($q, $timeout) {
+                return function(options) {
+                    var deferred = $q.defer();
+                    $timeout(function() {
+                        deferred.resolve(translationMock);
+                    });
+                    return deferred.promise;
+                };
+            });
+            $translateProvider.useLoader('i18nCustomLoader');
+        });
         inject(function($compile, $rootScope, $q, $timeout, $http) {
             compile = $compile;
             scope = $rootScope.$new();
@@ -36,31 +48,31 @@ describe('zakam-data-table', function() {
         });
         scope.mydata = [
             {
-                first : 'Yair',
-                last : 'Leviel',
-                id : 1234,
-                bu : "Luna",
+                first : 'Oliver',
+                last : 'Queen',
+                id : 11,
+                bu : "Justice League",
                 color: "Green",
                 birthday : new Date(2001,10,20),
-                generic : ["hello"]
+                generic : ["seriously whats wrong with arrow cave?"]
             },
             {
-                first : "Nick",
-                last: "Leon",
-                id : 2468,
-                bu : "Luna",
+                first : "Roy",
+                last: "Harper",
+                id : 20,
+                bu : "Teen Titans",
                 color:"Red",
                 birthday : new Date(2002,10,20),
-                generic : ["goodbye"]
+                generic : ["shoot em up"]
             },
             {
-                first: "K-Slice",
-                last:"McYoungPerson",
-                id:3141592653,
-                bu:"Luna",
-                color:"Yellow",
+                first: "Dinah",
+                last:"Lance",
+                id: 12,
+                bu:"Birds of Prey",
+                color:"Black",
                 birthday : new Date(2000,10,20),
-                generic : ["shake it off"]
+                generic : ["AAAAAAAAAAAAAAAAAAA"]
             }
         ];
         scope.columns = [
@@ -73,7 +85,7 @@ describe('zakam-data-table', function() {
             },
             {
                 content : 'id',
-                header : 'Employee ID'
+                header : 'ID'
             },
             {
                 content:"color",
@@ -81,7 +93,7 @@ describe('zakam-data-table', function() {
                 sort: function(){
                     var colorsValues = {
                         'Red' : 1,
-                        'Yellow' : 2,
+                        'Black' : 2,
                         'Green' : 3
                     };
                     return colorsValues[this.color];
@@ -107,7 +119,7 @@ describe('zakam-data-table', function() {
         afterEach(function() {
             document.body.removeChild(this.element);
         });
-        it('should show progress bar until fully rendered', function(){
+        /*it('should show progress bar until fully rendered', function(){
             var deferred = q.defer();
             scope.delayeddata = deferred.promise;
             timeout(function(){
@@ -122,9 +134,9 @@ describe('zakam-data-table', function() {
 
             expect(document.querySelector('akam-indeterminate-progress').getAttribute('completed')).to.match(/true/);
             expect(allRowsLoadedInTable).to.have.length(scope.mydata.length);
-        });
+        });*/
         it('should render all parts data table and pagination', function() {
-            var markup = '<akam-data-table data="mydata" schema="schema" filter-placeholder="yair"></akam-data-table>'
+            var markup = '<akam-data-table data="mydata" schema="columns" filter-placeholder="yair"></akam-data-table>'
             addElement(markup);
 
             var dataTableContainer = document.querySelector('div.akam-data-table');
@@ -135,7 +147,7 @@ describe('zakam-data-table', function() {
         });
         //it('should not have anything selected if checkboxes are active', function() {});
         it('should have filter be clear', function() {
-            var markup = '<akam-data-table data="mydata" schema="schema" filter-placeholder="yair"></akam-data-table>'
+            var markup = '<akam-data-table data="mydata" schema="columns" filter-placeholder="yair"></akam-data-table>'
             addElement(markup);
 
             var filterBox = document.querySelector(FILTER_BOX);
@@ -143,7 +155,7 @@ describe('zakam-data-table', function() {
             expect(filterBox.value).to.equal('');
         });
         it('should have 3 options for how many rows are displayed', function(){
-            var markup = '<akam-data-table data="mydata" schema="schema" filter-placeholder="yair"></akam-data-table>'
+            var markup = '<akam-data-table data="mydata" schema="columns" filter-placeholder="yair"></akam-data-table>'
             addElement(markup);
 
             var smallestPageSize = document.querySelector(PAGE_SIZE_SMALLEST);
@@ -155,12 +167,23 @@ describe('zakam-data-table', function() {
             expect(largestPageSize.textContent).to.match(/50/);
         });
     });
-    /*context('when data table is rendered', function(){
-        it('should display the total number of results', function(){});
-        it('should display action options on mouse hover of a row', function(){});
-        it('should show indeterminate progress bar when refreshed', function(){});
+    context('when data table is rendered', function(){
+        afterEach(function(){
+            document.body.removeChild(this.element);
+        });
+        it('should display the total number of results', function(){
+            var markup = '<akam-data-table data="mydata" schema="columns" filter-placeholder="yair"></akam-data-table>'
+            addElement(markup);
+
+            var totalItemsSpan = document.querySelector(TOTAL_ITEMS_SPAN);
+
+
+            expect(totalItemsSpan.textContent).to.contain('3');
+        });
+        //it('should display action options on mouse hover of a row', function(){});
+        //it('should show indeterminate progress bar when refreshed', function(){});
     });
-    context('when data table is loaded', function(){
+    /*context('when data table is loaded', function(){
         it('should be able to render without a toolbar', function(){});
         it('should be able to render without bulk delete', function(){});
         it('should be able to render without filter', function(){});
@@ -185,15 +208,110 @@ describe('zakam-data-table', function() {
     context('when deselecting an item', function(){
         it('should update deselected checkbox', function(){});
         it('should change background color of deselected items', function(){});
-    });
-    context('when interacting with column headers', function(){
-        it('should be able to sort alphabetically', function() {});
-        it('should be able to sort reverse-alphabetically', function() {});
-        it('should be able to sort numerically', function(){});
-        it('should be able to sort reverse-numerically', function(){});
-        it('should only be able to sort by one column at a time', function(){});
-        it('should be able to have no sort associated', function(){});
-    });
+    });*/
+    context('when interacting with sort options', function(){
+        beforeEach(function(){
+            var markup = '<akam-data-table data="mydata" schema="columns"></akam-data-table>';
+            addElement(markup);
+        });
+        afterEach(function() {
+            document.body.removeChild(this.element);
+        });
+        it('should be able to sort alphabetically', function() {
+            var sortByColumnOneAlphabectically = document.querySelectorAll(TABLE_COLUMN_HEADER)[0];
+            utilities.click(sortByColumnOneAlphabectically);
+            utilities.click(sortByColumnOneAlphabectically);
+
+            var rowOneColumnOne = document.querySelector(TABLE_ROW).querySelectorAll('td')[0];
+            var rowThreeColumnOne = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[0];
+
+            expect(rowOneColumnOne.textContent).to.match(/Dinah Lance/);
+            expect(rowThreeColumnOne.textContent).to.match(/Roy Harper/);
+        });
+        it('should be able to sort reverse-alphabetically', function() {
+            var sortByColumnOneAlphabectically = document.querySelectorAll(TABLE_COLUMN_HEADER)[0];
+            utilities.click(sortByColumnOneAlphabectically);
+
+            var rowOneColumnOne = document.querySelector(TABLE_ROW).querySelectorAll('td')[0];
+            var rowThreeColumnOne = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[0];
+
+            expect(rowOneColumnOne.textContent).to.match(/Roy Harper/);
+            expect(rowThreeColumnOne.textContent).to.match(/Dinah Lance/);
+        });
+        it('should be able to sort numerically', function(){
+            var sortByColumnTwoNumerically = document.querySelectorAll(TABLE_COLUMN_HEADER)[1];
+            utilities.click(sortByColumnTwoNumerically);
+
+            var rowOneColumnTwo = document.querySelector(TABLE_ROW).querySelectorAll('td')[1];
+            var rowThreeColumnTwo = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[1];
+
+            expect(rowOneColumnTwo.textContent).to.match(/11/);
+            expect(rowThreeColumnTwo.textContent).to.match(/20/);
+        });
+        it('should be able to sort reverse-numerically', function(){
+            var sortByColumnTwoNumerically = document.querySelectorAll(TABLE_COLUMN_HEADER)[1];
+            utilities.click(sortByColumnTwoNumerically);
+            utilities.click(sortByColumnTwoNumerically);
+
+            var rowOneColumnTwo = document.querySelector(TABLE_ROW).querySelectorAll('td')[1];
+            var rowThreeColumnTwo = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[1];
+
+            expect(rowOneColumnTwo.textContent).to.match(/20/);
+            expect(rowThreeColumnTwo.textContent).to.match(/11/);
+        });
+        it('should be able to sort by custom function', function(){
+            var sortByColumnThreeCustom = document.querySelectorAll(TABLE_COLUMN_HEADER)[2];
+            utilities.click(sortByColumnThreeCustom);
+
+            var rowOneColumnThree = document.querySelector(TABLE_ROW).querySelectorAll('td')[2];
+            var rowThreeColumnThree = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[2];
+
+            expect(rowOneColumnThree.textContent).to.match(/Red/);
+            expect(rowThreeColumnThree.textContent).to.match(/Green/);
+        });
+        it('should be able to sort by reverse custom function', function(){
+            var sortByColumnThreeCustom = document.querySelectorAll(TABLE_COLUMN_HEADER)[2];
+            utilities.click(sortByColumnThreeCustom);
+            utilities.click(sortByColumnThreeCustom);
+
+            var rowOneColumnThree = document.querySelector(TABLE_ROW).querySelectorAll('td')[2];
+            var rowThreeColumnThree = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[2];
+
+            expect(rowOneColumnThree.textContent).to.match(/Green/);
+            expect(rowThreeColumnThree.textContent).to.match(/Red/);
+        });
+        it('should be able to sort generically', function(){
+            var sortByColumnFiveGeneric = document.querySelectorAll(TABLE_COLUMN_HEADER)[4];
+            utilities.click(sortByColumnFiveGeneric);
+
+            var rowOneColumnFive = document.querySelector(TABLE_ROW).querySelectorAll('td')[4];
+            var rowThreeColumnFive = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[4];
+
+            expect(rowOneColumnFive.textContent).to.contain('["AAAAAAAAAAAAAAAAAAA"]');
+            expect(rowThreeColumnFive.textContent).to.contain('["shoot em up"]');
+        });
+        it('should be able to sort reverse generically', function(){
+            var sortByColumnFiveGeneric = document.querySelectorAll(TABLE_COLUMN_HEADER)[4];
+            utilities.click(sortByColumnFiveGeneric);
+            utilities.click(sortByColumnFiveGeneric);
+
+            var rowOneColumnFive = document.querySelector(TABLE_ROW).querySelectorAll('td')[4];
+            var rowThreeColumnFive = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[4];
+
+            expect(rowOneColumnFive.textContent).to.contain('["shoot em up"]');
+            expect(rowThreeColumnFive.textContent).to.contain('["AAAAAAAAAAAAAAAAAAA"]');
+        });
+        it('should sort entire rows', function(){
+
+            var firstRowColumnOne = document.querySelector(TABLE_ROW).querySelectorAll('td')[0];
+            var firstRowColumnTwo = document.querySelector(TABLE_ROW).querySelectorAll('td')[1];
+            var firstRowColumnThree = document.querySelector(TABLE_ROW).querySelectorAll('td')[2];
+
+            expect(firstRowColumnOne.textContent).to.match(/Dinah Lance/);
+            expect(firstRowColumnTwo.textContent).to.match(/12/);
+            expect(firstRowColumnThree.textContent).to.match(/Black/);
+        });
+    });/*
     context('when bulk deleting', function(){
         it('should have message box appear with confirmation of delete', function(){});
         it('should delete the chosen rows when confirmed', function(){});
@@ -214,15 +332,51 @@ describe('zakam-data-table', function() {
         it('should activate bubble help field if message over 40 characters', function(){});
         it('should highlight field where error is occuring', function(){});
         it('should accept edits when validation is complete', function(){});
-    });
+    });*/
     context('when navigating the data table', function(){
-        //ONLY ONE OF THESE IS GOING TO BE THE FINAL COURSE
-        it('should be able to scroll through all of the rows', function(){});
-        it('should be able to navigate through pagination', function(){});
-    });
+        it('should be able to navigate through pagination', function(){
+            scope.mydata = require('./bigTestingData/thousandUsers');
+            scope.colums = [
+                {
+                    content : function(){return this.first_name + ' ' + this.last_name;},
+                    header : 'Full Name',
+                    className : 'column-full-name'
+                },
+                {
+                    content : 'id',
+                    header : 'Emp. ID',
+                    className : 'column-employeeid'
+                }
+            ];
+            var markup = '<akam-data-table data="mydata" schema="columns"></akam-data-table>';
+            addElement(markup);
+        });
+        it('should highlight the clicked page', function() {
+            var fourthClickablePaginationIndex = this.element.querySelector(PAGINATION_INDEX_NTH+'(4)');
+
+            expect(fourthClickablePaginationIndex.classList.contains('active')).to.be.false;
+
+            utilities.click(fourthClickablePaginationIndex.querySelector('a'));
+            scope.$digest();
+
+            fourthClickablePaginationIndex = this.element.querySelector(PAGINATION_INDEX_NTH+'(4)');
+            expect(fourthClickablePaginationIndex.classList.contains('active')).to.be.true;
+        });
+        it('should change dislayed data appropriately', function(){
+            var fourthClickablePaginationIndex = this.element.querySelector(PAGINATION_INDEX_NTH+'(4)');
+
+            utilities.click(fourthClickablePaginationIndex.querySelector('a'));
+            scope.$digest();
+
+            var rowOneColumnTwo = document.querySelector(TABLE_ROW).querySelectorAll('td')[0];
+            var rowThreeColumnTwo = document.querySelectorAll(TABLE_ROW)[2].querySelectorAll('td')[0];
+            
+        });
+    });/*
     context('when interacting with the filter bar', function(){
         it('should filter based on input beginning-middle-end matches', function(){});
         it('should change text color of matching input', function(){});
+        it('should revert to pagination index 1', function(){});
     });
     context('when grouping items', function(){
         it('should display icon representing grouping of items',function(){});
