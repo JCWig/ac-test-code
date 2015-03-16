2a3b8f75eac7613be2cb5c1fafa932df2a49419c
Mike Sierra <msierra@akamai.com>
2015-03-16 09:22:28 -0400

    minor edit

--- src/list-box/index.js
+++ src/list-box/index.js
@@ -26,9 +26,9 @@ var angular = require('angular');
      * @restrict E
      *
      * @param {Array|Promise} data The array of data to show within
-     * the listbox.  If `data` is a promise, the indeterminate
-     * progress control displays in place of the contents until the
-     * promise is resolved or rejected.
+     * the listbox.  If `data` is a promise, an indeterminate progress
+     * control displays in place of the contents until the promise is
+     * resolved or rejected.
      *
      * @param {Array} columns The array of columns that describes the
      * schema to the list box layout and formatting.
