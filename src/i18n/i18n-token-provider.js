fcb38d7c4661eec7c74569b6aa69633bd31b5318
Mike Sierra <msierra@akamai.com>
2015-03-18 12:26:52 -0400

    reinsert removed close brce

--- src/i18n/i18n-token-provider.js
+++ src/i18n/i18n-token-provider.js
@@ -25,7 +25,7 @@ module.exports = function i18nTokenProvider(i18nConfig) {
                 path: i18nConfig.localeComponentPath,
                 app: false
             });
-
+        };
     /**
      * @ngdoc method
      *
