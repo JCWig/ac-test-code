4b203beb51fda2012c45c10de3ee393adc87dcee
D'Ambrosio, Michael <mdambros@akamai.com>
2015-03-19 19:36:55 -0400

    FEE-640 Fix component issues related to Billing Center integration. Elements with HTML in them will no longer show HTML when there is a match in the filter (i.e. the highlight filter gets a match). However, these elements will no longer be highlighted. This is a hard problem to solve because the highlight filter adds its own html to potentially already existing HTML. This makes supporting both highlighting arbitrary text and allowing for unsafe HTML to be bound to list-box and data-table difficult. A method would have to be written to walk to DOM tree and re-build the DOM structure that is passed in. This 'fix' is the lesser of the two evils (the other being showing HTML content in the table cell).

--- src/highlight/highlight.js
+++ src/highlight/highlight.js
@@ -1,18 +1,35 @@
 'use strict';
 
+var angular = require('angular');
+
 /* @ngInject */
 module.exports = function($sce) {
-    return function(text, phrase) {
-        if (!angular.isString(text)) {
-            text = String(text);
-        }
 
-        phrase = String(phrase).trim();
+  return highlight;
 
-        if (phrase){
-            text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>');
-        }
+  function highlight(text, phrase) {
+    phrase = phrase.trim();
 
-        return $sce.trustAsHtml(text);
-    };
+    var asDom = convertToDOM(text),
+        regexp = new RegExp('(' + phrase + ')', 'gi');
+
+    if (phrase && asDom.length === 0) {
+      text = text.replace(regexp, '<span class="highlighted">$1</span>');
+    }
+
+    return $sce.trustAsHtml(text);
+  }
+
+  function convertToDOM(text) {
+    var asDom;
+    try {
+      asDom = angular.element(text);
+    } catch(e) {
+      // the inputted text has some characters that jqlite don't recognize as a DOM string (or the input simply isn't wrapped)
+      asDom = angular.element('<span>' + text + '</span>');
+    }
+
+    return asDom;
+  }
+
 };
