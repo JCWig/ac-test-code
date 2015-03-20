995a489b9119865b9b6bce824f0d602a4baa117b
D'Ambrosio, Michael <mdambros@akamai.com>
2015-03-19 20:36:46 -0400

    FEE-640 Fix component issues related to Billing Center integration. Update to how we detect whether or not a string contains DOM elements.

--- src/highlight/highlight.js
+++ src/highlight/highlight.js
@@ -10,26 +10,16 @@ module.exports = function($sce) {
   function highlight(text, phrase) {
     phrase = phrase.trim();
 
-    var asDom = convertToDOM(text),
-        regexp = new RegExp('(' + phrase + ')', 'gi');
-
-    if (phrase && asDom.length === 0) {
-      text = text.replace(regexp, '<span class="highlighted">$1</span>');
+    if (phrase && !isDom(text)) {
+      text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>');
     }
 
     return $sce.trustAsHtml(text);
   }
 
-  function convertToDOM(text) {
-    var asDom;
-    try {
-      asDom = angular.element(text);
-    } catch(e) {
-      // the inputted text has some characters that jqlite don't recognize as a DOM string (or the input simply isn't wrapped)
-      asDom = angular.element('<span>' + text + '</span>');
-    }
-
-    return asDom;
+  // not a perfect method, but if there is an instance of "<" followed by at least one letter and then a ">", we return true
+  function isDom(text) {
+    return /<[A-Za-z]+.*>/.test(text);
   }
 
 };
