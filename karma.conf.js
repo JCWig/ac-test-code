d51384ac81d3c24f742e8988f797269d3fbbbd98
Kevin Kotosky <kkotosky@akamai.com>
2015-03-24 15:08:34 -0400

    removing jasmine-utils inclusion

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,7 @@ module.exports = function(config) {
             'node_modules/bootstrap/dist/css/bootstrap.css',
             'node_modules/moment/moment.js',
             'test/*.js',
-            'test/**/*.js',
-            'node_modules/jasmine-utils/src/jasmine-utils.js'
+            'test/**/*.js'
         ],
         frameworks: ['browserify', 'jasmine'],
         preprocessors: {
