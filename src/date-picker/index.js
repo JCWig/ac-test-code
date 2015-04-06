a7ecabbaab68af09ae91b007fa6b29d0e3a752b3
Leviel, Yair <yleviel@akamai.com>
2015-04-06 18:45:58 -0400

    Switch to using decorators for the month and day pickers to inject the correct scope variables we want to add.

--- src/date-picker/index.js
+++ src/date-picker/index.js
@@ -12,15 +12,72 @@ var angular = require('angular');
  *
  */
 module.exports = angular.module('akamai.components.date-picker', [
-        require('angular-bootstrap-npm'),
-        require('../i18n').name
+    require('angular-bootstrap-npm'),
+    require('../i18n').name
 ])
 
-.run(["$templateCache", function($templateCache) {
-  $templateCache.put("template/datepicker/datepicker.html", require('./templates/date-picker-switch.tpl.html'));
-  $templateCache.put("template/datepicker/month.html", require('./templates/date-picker-month-popup.tpl.html'));
-  $templateCache.put("template/datepicker/popup.html", require('./templates/date-picker-popup.tpl.html'));
-}])
+.config(function($provide) {
+  $provide.decorator('daypickerDirective', function($delegate) {
+    var directive = $delegate[0];
+    
+    directive.template = require('./templates/date-picker-day-popup.tpl.html');
+    directive.templateUrl = undefined;
+    
+    var link = directive.link;
+
+    directive.compile = function() {
+      return function(scope, element, attrs, ctrl) {
+        link.apply(this, arguments);
+        
+        //disable navigation according to the range
+        scope.daypickerNavPrevDisabled = function(){
+            var year = ctrl.activeDate.getFullYear(),
+                month = ctrl.activeDate.getMonth(),
+                firstDayOfMonth = new Date(year, month, 1);
+              
+            return ctrl.minDate && (firstDayOfMonth <= ctrl.minDate);
+        };
+        scope.daypickerNavNextDisabled = function() {
+            var year = ctrl.activeDate.getFullYear(),
+              month = ctrl.activeDate.getMonth(),
+              lastDayOfMonth = new Date(year, month+1, 0);
+          
+            return ctrl.maxDate && (lastDayOfMonth > ctrl.maxDate); };
+      };
+    };
+
+    return $delegate;
+  });
+})
+
+.config(function($provide) {
+  $provide.decorator('monthpickerDirective', function($delegate) {
+    var directive = $delegate[0];
+    
+    directive.template = require('./templates/date-picker-month-popup.tpl.html');
+    directive.templateUrl = undefined;
+    
+    var link = directive.link;
+
+    directive.compile = function() {
+      return function(scope, element, attrs, ctrl) {
+        link.apply(this, arguments);
+        
+        //disable navigation according to the range
+        scope.monthpickerNavPrevDisabled = function(){
+            var firstMonth = new Date(ctrl.activeDate.getFullYear(), 0, 1);
+            return ctrl.minDate && (firstMonth < ctrl.minDate);
+        };
+        scope.monthpickerNavNextDisabled =  function() {
+            var lastMonth = new Date(ctrl.activeDate.getFullYear(), 11, 1);
+            return ctrl.maxDate && (lastMonth > ctrl.maxDate);
+        };
+      };
+    };
+
+    return $delegate;
+  });
+})
 
 /**
  * @ngdoc directive
