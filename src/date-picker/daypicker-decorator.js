17f32c5c67b02c2ee3baa7f7e9d2fd5d48cb800f
Leviel, Yair <yleviel@akamai.com>
2015-04-07 12:16:06 -0400

    FEE-680 Addressed code review comments.  Added unit tests, updated logic a bit to handle for edge cases (last day of month max implies nav should be disabled going forward, etc.).  Cleaned up utilities file for spaces over tabs and linting.  Factored "find" method in utilities.

--- src/date-picker/daypicker-decorator.js
+++ src/date-picker/daypicker-decorator.js
@@ -1,35 +1,37 @@
 'use strict';
 
 module.exports = function($provide) {
-  $provide.decorator('daypickerDirective', function($delegate) {
-    var directive = $delegate[0];
-    
-    directive.template = require('./templates/date-picker-day-popup.tpl.html');
-    directive.templateUrl = undefined;
-    
-    var link = directive.link;
-
-    directive.compile = function() {
-      return function(scope, element, attrs, ctrl) {
-        link.apply(this, arguments);
+    $provide.decorator('daypickerDirective', function($delegate) {
+        // since: directives could potentially share names, the provider returns an array
+        // therefore: get the first item as we know we only have one.
+        var directive = $delegate[0];
         
-        //disable navigation according to the range
-        scope.daypickerNavPrevDisabled = function(){
-            var year = ctrl.activeDate.getFullYear(),
-                month = ctrl.activeDate.getMonth(),
-                firstDayOfMonth = new Date(year, month, 1);
-              
-            return ctrl.minDate && (firstDayOfMonth <= ctrl.minDate);
+        // override the default template for daypicker (template is evaluated before templateUrl)
+        directive.template = require('./templates/date-picker-day-popup.tpl.html');
+        directive.templateUrl = undefined;
+        
+        // reference the original link function
+        var link = directive.link;
+      
+        directive.compile = function() {
+            return function(scope, element, attrs, ctrl) {
+                link.apply(this, arguments);
+        
+                //disable navigation according to the range
+                scope.daypickerNavPrevDisabled = function(){
+                    var firstDayOfMonth = new Date(ctrl.activeDate.getFullYear(), ctrl.activeDate.getMonth(), 1);
+                    return ctrl.minDate && (firstDayOfMonth <= ctrl.minDate);
+                };
+                
+                scope.daypickerNavNextDisabled = function() {
+                    // calculate last day of month by using the 0th day trick:
+                    // if values are greater/lesser than their logical range, the adjacent value will be adjusted.
+                    var lastDayOfMonth = new Date(ctrl.activeDate.getFullYear(), ctrl.activeDate.getMonth() + 1, 0);
+                    return ctrl.maxDate && (lastDayOfMonth >= ctrl.maxDate);
+                };
+            };
         };
-        scope.daypickerNavNextDisabled = function() {
-            var year = ctrl.activeDate.getFullYear(),
-              month = ctrl.activeDate.getMonth(),
-              lastDayOfMonth = new Date(year, month+1, 0);
-          
-            return ctrl.maxDate && (lastDayOfMonth > ctrl.maxDate); };
-      };
-    };
-
-    return $delegate;
-  });
+      
+        return $delegate;
+    });
 };
