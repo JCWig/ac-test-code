1d9296a34569a94a44195bdcb05d1f481ca6c642
Kevin Kotosky <kkotosky@akamai.com>
2015-04-13 14:33:04 -0400

    Initial commit for tool-tip

--- src/tool-tip/tool-tip-directive.js
+++ src/tool-tip/tool-tip-directive.js
@@ -1,39 +1,64 @@
 'use strict';
 
 /* @ngInject */
-module.exports = function($log, $position) {
+module.exports = function($log, $position, $compile) {
     return {
-        restrict: 'E',
+        restrict: 'A',
         replace : true,
-        transclude :true,
         scope :{
-            text : "@",
-            triggerText: "@",
-
+            placement: "@",
+            header:"@",
+            trigger:"@",
+            content: "@"
         }, 
-        template: require('./templates/tool-tip.tpl.html'), //'<a href="#" ng-transclude tooltip="{{text}}">{{ hoverText }}</a>',
-        link: function(scope, element, attrs) {
+        //template: require('./templates/tool-tip-2.tpl.html'), 
+        link: function(scope, element, attrs) {            
+            scope.opened = false;
             scope.popupDelay = 500;
-            scope.animation = true;
-            scope.placement = "bottom";
-            console.log("HELLO");
+            scope.animation = true; 
+            
+            var content = require('./templates/tool-tip-2.tpl.html');
+            element.append($compile(content)(scope));
+            var toolTip = element.children('.tool-tip');
+            setCoords();
 
-            /*scope.trigger = function($event) {
-                $event.preventDefault();
-                $event.stopPropagation();
-                scope.close();
-            };
-            if(scope.text.length() > 60){
-                //FAIL SOMEHOW? Just shorten message to 60 characters? WHAT DO I DO JOHNNY WHAT DO I DO?
-                //Default take the first 60 characters
-                scope.text = scope.text.splice(0,59);
+            if(scope.trigger === "click"){
+                element.on("click", function(){
+                    scope.toggle();
+                });
+            } else {
+                element.on("mouseover", function(){
+                    scope.toggle();
+                });
             }
-            scope.close = function(){
-                scope.closing = true;
-                $timeout(function(){
-                    element.remove();
-                }, 500);
-            };*/
+            
+            scope.isOpen = function(){
+                return scope.opened;
+            }
+            scope.toggle = function(){
+                //setCoords();
+                scope.opened = !scope.opened;
+                console.log
+                toolTip.toggleClass('in', scope.opened);
+            }
+            scope.isAnimation = function(){
+                return scope.animation;
+            }
+            function setCoords(){
+                if(scope.placement === 'bottom'){
+                    scope.myLeft = (element[0].offsetLeft - toolTip.clientWidth / 2 + (element[0].clientWidth / 2)) + "px";
+                    scope.myTop = (element[0].offsetTop + element[0].clientHeight + 5) + "px"; 
+                } else if (scope.placement === 'top'){
+                    scope.myLeft = (element[0].offsetLeft - toolTip.clientWidth / 2 + (element[0].clientWidth / 2)) + "px";
+                    scope.myTop = (element[0].offsetTop  - toolTip.clientHeight - element[0].clientHeight/2 - 5) + "px"; 
+                } else if(scope.placement === 'right'){
+                    scope.myLeft = (element[0].offsetLeft + element[0].clientWidth + 5) + "px";
+                    scope.myTop = (element[0].offsetTop + (element[0].clientHeight / 2) - (toolTip.clientHeight / 2)) + "px";
+                } else if(scope.placement === 'left'){
+                    scope.myLeft = (element[0].offsetLeft - toolTip.clientWidth - element[0].clientWidth/2 - 5) + "px";
+                    scope.myTop = (element[0].offsetTop + (element[0].clientHeight / 2) - (toolTip.clientHeight / 2)) + "px";     
+                }
+            }
         }
     };
 };
