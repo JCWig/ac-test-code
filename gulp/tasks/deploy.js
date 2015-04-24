8c1502aeda0c3be76c1635804842161b522c381a
Leviel, Yair <yleviel@akamai.com>
2015-04-24 10:02:21 -0400

    Instead use Jenkins Git Plugin variables to determine branch info.

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,36 +1,29 @@
 var gulp      = require('gulp');
 var shell = require('shelljs');
 var rsync = require('rsyncwrapper').rsync;
-var globby = require('globby');
-var config    = require('../config').production;
 var bundleLogger = require('../util/bundle-logger');
 
 gulp.task('deploy', function(){
-    var output = shell.exec('git rev-parse --symbolic-full-name --abbrev-ref @{u}');
-    bundleLogger.log(output);
+    var result = shell.exec('echo $GIT_BRANCH');
+    var symbolicName = result.output.trim();
+    bundleLogger.log('symbolic name', symbolicName);
     
-    /*
-    git.revParse({args:'--symbolic-full-name --abbrev-ref @{u}'}, function (err, branchName) {
-        bundleLogger.log('current git branch: '+ branchName);
-        //clean up branch name:
-        var cleanBranchName = branchName.replace('origin/feature/', '').replace(' ', '_');
-        bundleLogger.log('clean branch name: '+ cleanBranchName);
+    //clean up branch name:
+    var cleanBranchName = symbolicName.replace('origin/feature/', '').replace(' ', '_');
+    bundleLogger.log('clean branch name: '+ cleanBranchName);
 
-        var longFolderName = '/315289/website/branches/' + cleanBranchName;
+    var longFolderName = '/315289/website/branches/' + cleanBranchName;
 
-        bundleLogger.log('rsync destination: '+ longFolderName);
-        return;
-        //TODO: Handle scenarios where the folder needs to be generated on the server side
-        rsync({
-          ssh: true,
-          src: ['./dist', './examples'],
-          dest: 'sshacs@lunahome.upload.akamai.com:' + longFolderName,
-          recursive: true,
-          args: ["--copy-dirlinks", "--verbose", "--compress"]
-          //dryRun: true
-        }, function(error, stdout, stderr, cmd) {
-            bundleLogger.log(error, stdout, cmd);
-        });
+    bundleLogger.log('rsync destination: '+ longFolderName);
+    //TODO: Handle scenarios where the folder needs to be generated on the server side
+    rsync({
+      ssh: true,
+      src: ['./dist', './examples'],
+      dest: 'sshacs@lunahome.upload.akamai.com:' + longFolderName,
+      recursive: true,
+      args: ["--copy-dirlinks", "--verbose", "--compress"]
+      //dryRun: true
+    }, function(error, stdout, stderr, cmd) {
+        bundleLogger.log(error, stdout, cmd);
     });
-    */
 });
