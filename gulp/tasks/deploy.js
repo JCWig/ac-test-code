7884e786f9fdf7d3c783fa01bae8dd86dda6e159
Leviel, Yair <yleviel@akamai.com>
2015-04-24 09:35:08 -0400

    FEE-705 Change module from gulp-git to shelljs to execute the command.  Updated the command to use symbolic full name.

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,21 +1,25 @@
 var gulp      = require('gulp');
-var git       = require('gulp-git');
+var shell = require('shelljs');
 var rsync = require('rsyncwrapper').rsync;
 var globby = require('globby');
 var config    = require('../config').production;
 var bundleLogger = require('../util/bundle-logger');
 
 gulp.task('deploy', function(){
-    git.revParse({args:'--abbrev-ref HEAD'}, function (err, branchName) {
+    var output = shell.exec('git rev-parse --symbolic-full-name --abbrev-ref @{u}');
+    bundleLogger.log(output);
+    
+    /*
+    git.revParse({args:'--symbolic-full-name --abbrev-ref @{u}'}, function (err, branchName) {
         bundleLogger.log('current git branch: '+ branchName);
         //clean up branch name:
-        var cleanBranchName = branchName.replace('feature/', '').replace(' ', '_');
+        var cleanBranchName = branchName.replace('origin/feature/', '').replace(' ', '_');
         bundleLogger.log('clean branch name: '+ cleanBranchName);
 
         var longFolderName = '/315289/website/branches/' + cleanBranchName;
 
         bundleLogger.log('rsync destination: '+ longFolderName);
-
+        return;
         //TODO: Handle scenarios where the folder needs to be generated on the server side
         rsync({
           ssh: true,
@@ -28,4 +32,5 @@ gulp.task('deploy', function(){
             bundleLogger.log(error, stdout, cmd);
         });
     });
+    */
 });
