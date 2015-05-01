b599e020a44d34b8aaee970440f107fd8d41d59a
Shawn Dahlen <shawn@dahlen.me>
2015-05-01 15:03:10 -0400

    Bump minor release to 0.7.0

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,4 +9,4 @@ var angular = require('angular');
  */
 module.exports = angular.module('akamai.components.utils', [])
   //TODO: Remember to update this if the version in package.json ever changes
-  .constant('VERSION', '0.6.0');
+  .constant('VERSION', '0.7.0');
