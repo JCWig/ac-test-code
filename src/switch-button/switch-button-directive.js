5403a4d965f5234de97d4408323826387ceadff9
Miguel Bollar <mbollar@akamai.com>
2015-05-04 16:26:06 -0400

    Added scaffold for switch-button directive

---
+++ src/switch-button/switch-button-directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* @ngInject */
+module.exports = function() {
+
+  return {
+    restrict: 'E',
+    scope: {}
+
+    //link: function(scope, elem, attrs) {
+    //
+    //}
+  };
+};
