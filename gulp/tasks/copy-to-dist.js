f0b845c50c781d206c9d6dc75654b6d6c41f5a4d
D'Ambrosio, Michael <mdambros@akamai.com>
2015-05-08 13:52:18 -0400

    FEE-767 Mega menu sprite map not being included in akamai-core images dir. Now they are.

--- gulp/tasks/copy-to-dist.js
+++ gulp/tasks/copy-to-dist.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var config = require('../config').production;
 
 gulp.task('copy-to-dist', function() {
-  gulp.src('assets/locales/**', {
+  gulp.src([
+    'assets/locales/**',
+    'assets/images/head_sprite.png',
+    'assets/images/search-icons.png'
+  ], {
     base: 'assets'
   })
     .pipe(gulp.dest(config.dest));
