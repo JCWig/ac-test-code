2a93c4e698167b74f115f7ef41e28013fb468553
Ryan Silva <rysilva@gmail.com>
2015-05-18 12:01:11 -0400

    FEE-763 Custom markup in panel header

--- src/content-panel/content-panel-directive.js
+++ src/content-panel/content-panel-directive.js
@@ -12,13 +12,29 @@ module.exports = function() {
       onToggle: '&?'
     },
     template: require('./templates/content-panel.tpl.html'),
-    link: function(scope) {
-      scope.isCollapsed = !!scope.isCollapsed;
-      scope.$watch('isCollapsed', function(newValue, oldValue) {
-        if (newValue !== oldValue && typeof scope.onToggle === 'function') {
-          scope.onToggle({value: newValue});
+    compile:  function compile(tElement, tAttrs) {
+      //If the header attribute is not specified, assume that the developer provided their own akam-content-panel-header
+      // and akam-content-panel-body inner directives.
+      if ( !tAttrs.header ) {
+        tElement.empty();
+      }
+
+      return {
+        pre: function preLink(scope, iElement, iAttrs, controller, transclude) {
+          //This is essentially the same as if this directive's template had ng-transclude on the root element.
+          if ( !iAttrs.header ) {
+            iElement.append(transclude());
+          }
+        },
+        post: function postLink(scope, iElement, iAttrs, controller) {
+          scope.isCollapsed = !!scope.isCollapsed;
+          scope.$watch('isCollapsed', function(newValue, oldValue) {
+            if (newValue !== oldValue && typeof scope.onToggle === 'function') {
+              scope.onToggle({value: newValue});
+            }
+          });
         }
-      });
+      }
     }
   };
 };
