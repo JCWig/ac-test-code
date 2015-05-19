1c41f2b736fcb2a612e3095fb538916b099c31dd
Ryan Silva <rysilva@gmail.com>
2015-05-19 10:34:01 -0400

    Fix eslint issues

--- src/content-panel/content-panel-body-directive.js
+++ src/content-panel/content-panel-body-directive.js
@@ -2,10 +2,10 @@
 
 /* @ngInject */
 module.exports = function() {
-    return {
-        restrict: 'E',
-        transclude: true,
-        replace: true,
-        template: require('./templates/content-panel-body.tpl.html')
-    };
+  return {
+    restrict: 'E',
+    transclude: true,
+    replace: true,
+    template: require('./templates/content-panel-body.tpl.html')
+  };
 };
--- src/content-panel/content-panel-directive.js
+++ src/content-panel/content-panel-directive.js
@@ -12,8 +12,9 @@ module.exports = function() {
       onToggle: '&?'
     },
     template: require('./templates/content-panel.tpl.html'),
-    compile:  function compile(tElement, tAttrs) {
-      //If the header attribute is not specified, assume that the developer provided their own akam-content-panel-header
+    compile: function compile(tElement, tAttrs) {
+      //If the header attribute is not specified, assume
+      // the developer provided their own akam-content-panel-header
       // and akam-content-panel-body inner directives.
       if ( !tAttrs.header ) {
         tElement.empty();
@@ -21,12 +22,13 @@ module.exports = function() {
 
       return {
         pre: function preLink(scope, iElement, iAttrs, controller, transclude) {
-          //This is essentially the same as if this directive's template had ng-transclude on the root element.
+          //This is essentially the same as if this directive's template had
+          // ng-transclude on the root element.
           if ( !iAttrs.header ) {
             iElement.append(transclude());
           }
         },
-        post: function postLink(scope, iElement, iAttrs, controller) {
+        post: function postLink(scope) {
           scope.isCollapsed = !!scope.isCollapsed;
           scope.$watch('isCollapsed', function(newValue, oldValue) {
             if (newValue !== oldValue && typeof scope.onToggle === 'function') {
@@ -34,7 +36,7 @@ module.exports = function() {
             }
           });
         }
-      }
+      };
     }
   };
 };
