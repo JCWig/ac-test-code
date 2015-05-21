656498e41a6ae84c1b747171591d369658abf2c4
Kevin Kotosky <kkotosky@akamai.com>
2015-05-21 11:01:02 -0400

    FEE-724 Added in handling of list box de/select all. If any case arises where not everything is selected de/select all is unchecked. If at any time everything is selected de/select all is checked

--- src/list-box/list-box-directive.js
+++ src/list-box/list-box-directive.js
@@ -110,6 +110,9 @@ module.exports = function($log, $q, $timeout, uuid, $filter, translate) {
         scope.dataTable = orderBy(scope.dataTable,
           scope.state.sortInfo.predicate,
           scope.state.sortInfo.reverseSort);
+        if (scope.dataTable) {
+          manageStates(scope);
+        }
       };
 
       scope.processDataTable = function() {
@@ -171,6 +174,7 @@ module.exports = function($log, $q, $timeout, uuid, $filter, translate) {
           scope.sortColumn(autoSortableColumns[0]);
         }
         scope.loading = false;
+        manageStates(scope);
       };
 
       scope.$watch('data', function() {
@@ -195,7 +199,6 @@ module.exports = function($log, $q, $timeout, uuid, $filter, translate) {
           $timeout(function() {
             scope.processDataTable();
           }, 0);
-
         }).catch(function() {
           scope.failed = true;
         });
@@ -254,6 +257,7 @@ module.exports = function($log, $q, $timeout, uuid, $filter, translate) {
             });
           }
         }
+        manageStates(scope);
         scope.onChange({value: scope.selectedItems});
       };
 
@@ -422,4 +426,13 @@ function getColumnSortClass(column, sortedColumn, reverseSort) {
   }
 
   return 'column-sortable column-sorted ' + (reverseSort ? 'desc' : 'asc');
+}
+
+function manageStates(scope) {
+  var selectedItems = scope.dataTable.filter(function(item) {
+    return item.selected;
+  });
+
+  scope.state.allSelected = scope.dataTable.length ?
+    selectedItems.length === scope.dataTable.length : false;
 }
