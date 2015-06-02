34100ab0c94decf9c931520599b76f939bbf1725
D'Ambrosio, Michael <mdambros@akamai.com>
2015-06-02 14:06:02 -0400

    FEE-710 Implement support for custom markup in data table. Fixed eslint errors in the utilities test and made the build pass (by deleting old data-table tests).

--- spec/utils/akam-standalone-directive.js
+++ spec/utils/akam-standalone-directive.js
@@ -1,56 +1,55 @@
+/* globals angular, beforeEach, afterEach */
 'use strict';
+
 var utilities = require('../utilities');
 
 var TREE_VIEW_WRAPPER = '.akam-tree-view';
-var DATA_TABLE_WRAPPER = '.akam-data-table';
 
 describe('akamai.components.akam-standalone', function() {
-    var scope, timeout, compile, q;
-    beforeEach(function() {
-        inject.strictDi(true);
-        var self = this;
-        angular.mock.module(require('../../src/utils').name);
-        angular.mock.module(require('../../src/data-table').name);
-        angular.mock.module(require('../../src/tree-view').name);
-        inject(function($compile, $rootScope, $httpBackend) {
-            scope = $rootScope.$new();
-            compile = $compile;
-            $httpBackend.when('GET', utilities.LIBRARY_PATH).respond({});
-            $httpBackend.when('GET', utilities.CONFIG_PATH).respond({});
-            $httpBackend.flush();
-        });
+  var self, scope, compile;
+
+  beforeEach(function() {
+    self = this;
+    angular.mock.inject.strictDi(true);
+    angular.mock.module(require('../../src/utils').name);
+    angular.mock.module(require('../../src/tree-view').name);
+    angular.mock.inject(function($compile, $rootScope, $httpBackend) {
+      scope = $rootScope.$new();
+      compile = $compile;
+      $httpBackend.when('GET', utilities.LIBRARY_PATH).respond({});
+      $httpBackend.when('GET', utilities.CONFIG_PATH).respond({});
     });
-    afterEach(function() {
-        if(self.element){
-            document.body.removeChild(self.element);
-            self.element = null;
-        }
+  });
+
+  afterEach(function() {
+    if (self.element) {
+      document.body.removeChild(self.element);
+      self.element = null;
+    }
+  });
+
+  function addElement(markup) {
+    self.el = compile(markup)(scope);
+    scope.$digest();
+    self.element = document.body.appendChild(self.el[0]);
+  }
+
+  describe('when rendering', function() {
+    it('should add correct class when directive is not replaced', function() {
+      var markup, treeContents;
+
+      scope.contextData = {
+        parent: {title: 'Justice League'},
+        current: {title: 'Bruce Wayne'},
+        children: [{title: 'Dick Grayson'}]
+      };
+      scope.triggerChange = function() {};
+      markup = '<div style="max-width:150px"><akam-tree-view akam-standalone ' +
+        'context-data="contextData" on-context-change="triggerChange"> </akam-tree-view></div>';
+      addElement(markup);
+      treeContents = document.querySelector(TREE_VIEW_WRAPPER);
+      expect(treeContents.classList.contains('standalone')).toBe(true);
     });
-    function addElement(markup) {
-        self.el = compile(markup)(scope);
-        scope.$digest();
-        self.element = document.body.appendChild(self.el[0]);
-      }
-    describe('when rendering', function(){
-        it('should add correct class when directive is not replaced', function(){
-            scope.contextData = {
-                parent:{title:"Justice League"},
-                current:{title: "Bruce Wayne"},
-                children:[{title:"Dick Grayson"}]
-            };
-            scope.triggerChange = function(){};
-            var markup = '<div style="max-width:150px"><akam-tree-view akam-standalone context-data="contextData" on-context-change="triggerChange"> </akam-tree-view></div>';
-            addElement(markup);
-            var treeContents = document.querySelector(TREE_VIEW_WRAPPER);
-            expect(treeContents.classList.contains('standalone')).toBe(true);
-        });
-        it('should add correct class when directive is replaced', function(){
-            scope.data = [];
-            scope.schema = [];
-            var markup = '<akam-data-table data="data" schema="schema" akam-standalone> </akam-data-table>';
-            addElement(markup);
-            var datatableConents = document.querySelector(DATA_TABLE_WRAPPER);
-            expect(datatableConents.classList.contains('standalone')).toBe(true);
-        });
-    });
+
+  });
 });
