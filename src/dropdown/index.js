782b40ad6f9a8037883fb5a6c7dde89368656bc3
Miguel Bollar <mbollar@akamai.com>
2015-06-04 14:44:53 -0400

    added UI.Utils highlight
    FEE-691

--- src/dropdown/index.js
+++ src/dropdown/index.js
@@ -1,8 +1,11 @@
 'use strict';
 var angular = require('angular');
+require('../../node_modules/angular-ui-utils/modules/highlight/highlight.js');
 
 module.exports = angular.module('akamai.components.dropdown', [
-  require('angular-bootstrap-npm')
+  require('angular-bootstrap-npm'),
+  'ngSanitize',
+  'ui.highlight'
 ])
   .service('dropdownTransformer', require('./dropdown-transformer'))
 
