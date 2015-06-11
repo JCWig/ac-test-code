42c783a7b43e6285481e61f966beaf34e89617ff
Yair Leviel <yleviel@akamai.com>
2015-06-11 18:14:26 -0400

    FEE-772 Better unit test, added constant block for config info.

--- src/auth/index.js
+++ src/auth/index.js
@@ -13,6 +13,16 @@ var angular = require('angular');
 module.exports = angular.module('akamai.components.auth', [
   require('../utils').name
 ])
+  .constant('configuration', {
+    //client id we are using in authN
+    clientId: '0ad3c5bc-fb66-4fa0-b94a-1bf712eae628',
+    //token endpoint
+    tokenUrl: '/ids-authn/v1/oauth2/token',
+    //token introspection endpoint
+    introspectionUrl: '/ids-authn/v1/introspect',
+    //url pointing to luna logout
+    lunaLogoutUrl: '/portal/logout.jsp'
+  })
   .factory('httpBuffer', require('./http-buffer-service'))
   .factory('token', require('./token-service'))
   .factory('authInterceptor', require('./auth-interceptor'))
