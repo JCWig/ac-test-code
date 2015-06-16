6b1fe5e18d77b5680b4c40eadf82aecc4c86f212
Kevin Kotosky <kkotosky@akamai.com>
2015-06-16 17:04:03 -0400

    FEE-710 added in data table tests and updated a few edge cases.

--- src/table/table-template-service.js
+++ src/table/table-template-service.js
@@ -48,38 +48,41 @@ module.exports = function($log) {
       template += '<th class="column-checkbox"></th>';
     }
 
-    angular.forEach(element.children, function(elem) {
-      // clone the element because we will be modifying it with setAttribute and classList
-      var elemClone = angular.element(elem).clone()[0],
-        tpl;
+    if (!element || element.children.length === 0) {
+      $log.warn('Expected "akam-table-column" tag found nothing');
+    } else {
+      angular.forEach(element.children, function(elem) {
+        // clone the element because we will be modifying it with setAttribute and classList
+        var elemClone = angular.element(elem).clone()[0],
+          tpl;
 
-      if (elemClone.nodeName.toLowerCase() !== 'akam-table-column') {
-        $log.warn('Expected "akam-table-column" tag, found', elemClone.nodeName.toLowerCase());
-        return;
-      }
+        if (elemClone.nodeName.toLowerCase() !== 'akam-table-column') {
+          $log.warn('Expected "akam-table-column" tag, found', elemClone.nodeName.toLowerCase());
+          return;
+        }
 
-      // set up ng-click and ng-class for handling sorting
-      if (isSortable(elemClone, attributes)) {
-        elemClone.classList.add(sortableClass);
+        // set up ng-click and ng-class for handling sorting
+        if (isSortable(elemClone, attributes)) {
+          elemClone.classList.add(sortableClass);
 
-        elemClone.setAttribute('ng-class', 'table.sortDirectionClass("' +
-          elemClone.getAttribute('row-property') + '")');
+          elemClone.setAttribute('ng-class', 'table.sortDirectionClass("' +
+            elemClone.getAttribute('row-property') + '")');
 
-        elemClone.setAttribute('ng-click', 'table.sortColumn("' +
-          elemClone.getAttribute('row-property') + '")');
-      }
+          elemClone.setAttribute('ng-click', 'table.sortColumn("' +
+            elemClone.getAttribute('row-property') + '")');
+        }
 
-      headerHtml = '<span akam-translate="' +
-        elemClone.getAttribute('header-name') +
-        '"></span><i></i>';
+        headerHtml = '<span akam-translate="' +
+          elemClone.getAttribute('header-name') +
+          '"></span><i></i>';
 
-      tpl = elemClone.outerHTML.replace('<akam-table-column', '<th');
-      tpl = tpl.replace('akam-table-column>', 'th>');
-      tpl = angular.element(tpl).html(headerHtml)[0].outerHTML;
+        tpl = elemClone.outerHTML.replace('<akam-table-column', '<th');
+        tpl = tpl.replace('akam-table-column>', 'th>');
+        tpl = angular.element(tpl).html(headerHtml)[0].outerHTML;
 
-      template += tpl;
-    });
-
+        template += tpl;
+      });
+    }
     return template;
   }
 
@@ -100,39 +103,42 @@ module.exports = function($log) {
       '<label for="{{ table.id + \'-item-\' + $index }}"></label></td>';
     }
 
-    angular.forEach(element.children, function(elem) {
-      var tpl, content;
+    if (!element || element.children.length === 0) {
+      $log.warn('Expected "akam-table-row" tag found nothing');
+    } else {
+      angular.forEach(element.children, function(elem) {
+        var tpl, content;
 
-      if (elem.nodeName.toLowerCase() !== 'akam-table-column') {
-        $log.warn('Expected "akam-table-column" tag, found', elem.nodeName.toLowerCase());
-        return;
-      }
+        if (elem.nodeName.toLowerCase() !== 'akam-table-column') {
+          $log.warn('Expected "akam-table-column" tag, found', elem.nodeName.toLowerCase());
+          return;
+        }
 
-      // a monstrosity that is used to determine if the row is sortable or filterable and a row
-      // property isn't defined.
-      if (!elem.hasAttribute('row-property') &&
-        (!angular.isDefined(attributes.noSort) && !elem.hasAttribute('no-sort') ||
-        !angular.isDefined(attributes.noFilter) && !elem.hasAttribute('no-filter'))) {
+        // a monstrosity that is used to determine if the row is sortable or filterable and a row
+        // property isn't defined.
+        if (!elem.hasAttribute('row-property') &&
+          (!angular.isDefined(attributes.noSort) && !elem.hasAttribute('no-sort') ||
+          !angular.isDefined(attributes.noFilter) && !elem.hasAttribute('no-filter'))) {
 
-        $log.debug('', elem, ' has no "row-property" attribute defined. The column will' +
-        'be neither filterable nor sortable. Add "no-filter" and "no-sort" to suppress this ' +
-        'message.');
-      }
+          $log.debug('', elem, ' has no "row-property" attribute defined. The column will' +
+          'be neither filterable nor sortable. Add "no-filter" and "no-sort" to suppress this ' +
+          'message.');
+        }
 
-      tpl = elem.outerHTML.replace('<akam-table-column', '<td');
-      tpl = tpl.replace('akam-table-column>', 'td>');
+        tpl = elem.outerHTML.replace('<akam-table-column', '<td');
+        tpl = tpl.replace('akam-table-column>', 'td>');
 
-      if (elem.innerHTML.trim()) {
-        content = elem.innerHTML;
-      } else {
-        content = '{{ row.' + elem.getAttribute('row-property') + '}}';
-      }
+        if (elem.innerHTML.trim()) {
+          content = elem.innerHTML;
+        } else {
+          content = '{{ row.' + elem.getAttribute('row-property') + '}}';
+        }
 
-      tpl = angular.element(tpl).html(content)[0].outerHTML;
+        tpl = angular.element(tpl).html(content)[0].outerHTML;
 
-      template += tpl;
-    });
-
+        template += tpl;
+      });
+    }
     return template;
   }
 
