8206ffc79791ec0991fe5f6e425636b700b6f2af
Miguel Bollar <mbollar@akamai.com>
2015-06-19 15:01:22 -0400

    FEE-771 - added isValid and ng-disabled to the 'Next' button

--- src/wizard/wizard-content-directive.js
+++ src/wizard/wizard-content-directive.js
@@ -1,12 +1,18 @@
 'use strict';
 
 /* @ngInject */
-module.exports = function($compile, $http, $templateCache, $q) {
+module.exports = function($compile, $http, $templateCache, $q, $log) {
 
   return {
     restrict: 'E',
-    transclude: true,
-    template: '<div class="modal-body" ng-transclude></div>'
+    template: '<div class="modal-body"><div></div></div>',
+    link: function(scope, element) {
 
+      scope.$watch('stepIndex', function(stepIndex) {
+        $log.log('in wizard-content-directive link function');
+        element.html(($compile(scope.steps[scope.stepIndex].template)(scope.contentScope))[0].outerHTML);
+
+      });
+    }
   };
 };
--- src/wizard/wizard-service.js
+++ src/wizard/wizard-service.js
@@ -3,11 +3,16 @@
 var angular = require('angular');
 
 /* @ngInject */
-module.exports = function($templateCache, $log, $modal) {
+module.exports = function($templateCache, $log, $modal, $controller) {
 
   return {
     open: function(options) {
-      var wizardScope = options.scope;
+      var wizardScope = options.scope.$new();
+      wizardScope.contentScope = options.scope;
+      delete options.scope;
+
+      $controller(options.controller, {$scope: wizardScope.contentScope});
+      delete options.controller;
 
       options = angular.extend(options, {
         title: 'Wizard Example',
@@ -18,45 +23,63 @@ module.exports = function($templateCache, $log, $modal) {
         steps: [
           {
             name: 'Step 1',
-            templateId: 'step1'
+            templateId: 'step1',
+            validate: function(scope) {
+              $log.log('step1 validate', scope);
+              return true;
+            }
           },
           {
             name: 'Step 2',
-            templateId: 'step2'
+            templateId: 'step2',
+            validate: function(scope) {
+              $log.log('step2 validate', scope);
+              return true;
+            }
+
           },
           {
             name: 'Step 3',
-            templateId: 'step3'
+            templateId: 'step3',
+            validate: function(scope) {
+              $log.log('step3 validate', scope);
+              return false;
+            }
+
           },
           {
             name: 'Step 4',
-            templateId: 'step4'
+            templateId: 'step4',
+            validate: function(scope) {
+              $log.log('step4 validate', scope);
+              return true;
+            }
+
           }
         ]
       });
-
-      var scope = options.scope;
 
       for (var i = 0; i < options.steps.length; i++) {
         options.steps[i].template = $templateCache.get(options.steps[i].templateId);
       }
 
-      scope.steps = options.steps;
-      scope.stepIndex = 0;
+      wizardScope.steps = options.steps;
+      wizardScope.stepIndex = 0;
 
-      scope.previousStep = function() {
-        scope.stepIndex--;
+      wizardScope.previousStep = function() {
+        wizardScope.stepIndex--;
       };
 
-      scope.nextStep = function(validate) {
-        if (validate) {
-          validate(scope.stepIndex);
-        }
-        scope.stepIndex++;
+      wizardScope.nextStep = function() {
+        wizardScope.stepIndex++;
+      };
+
+      wizardScope.isValid = function() {
+        return wizardScope.steps[wizardScope.stepIndex].validate(wizardScope.contentScope);
       };
 
       var instance = $modal.open(angular.extend(options, {
-        scope: scope,
+        scope: wizardScope,
         template: require('./templates/wizard.tpl.html')
       }));
 
