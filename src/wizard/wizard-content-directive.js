bbca33673b54cbb10cdec51b48992d6056e765e6
Miguel Bollar <mbollar@akamai.com>
2015-06-23 13:29:13 -0400

    FEE-771: Static markup - step navigation

--- src/wizard/wizard-content-directive.js
+++ src/wizard/wizard-content-directive.js
@@ -6,8 +6,8 @@ module.exports = function($compile, $http, $templateCache, $q, $log) {
   return {
     restrict: 'E',
     template: '<div class="modal-body"><div></div></div>',
-    link: function(scope, element) {
 
+    link: function(scope, element) {
       scope.$watch('currentStep', function(currentStep) {
         $log.log('in wizard-content-directive link function');
         element.html(($compile(currentStep.template)(scope.contentScope))[0].outerHTML);
