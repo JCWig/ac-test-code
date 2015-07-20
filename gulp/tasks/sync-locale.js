5c4e43e4e50d21fff6d2c81c8febe69f7afe89d5
Yair Leviel <yleviel@akamai.com>
2015-07-20 11:12:52 -0400

    FEE-826 Fail build on sync-locale errors

--- gulp/tasks/sync-locale.js
+++ gulp/tasks/sync-locale.js
@@ -2,8 +2,12 @@
 
 var gulp = require('gulp');
 var shell = require('shelljs');
+var log = require('gulp-util').log;
 
 // Gulp task to run sync-locale test to generate xunitreport
 gulp.task('sync-locale', function() {
-    shell.exec('npm run locale-jenkins');
+  if (shell.exec('npm run locale-jenkins').code !== 0) {
+    log('errors found in locales files, exiting');
+    shell.exit(1);
+  }
 });
