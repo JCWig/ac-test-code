3419e08317f8c89b235c1039f92dc563698edcf4
sewang <sewang@akamai.com>
2015-07-24 11:02:40 -0400

    FEE-1019 added not-collapsable descriptions in the index file

--- src/content-panel/index.js
+++ src/content-panel/index.js
@@ -39,10 +39,16 @@ module.exports = angular.module('akamai.components.content-panel', [
  * @restrict E
  *
  * showing expand/collapse icon, otherwise icon will not hidden to user.
- * @param {Boolean} [isCollapsed=true] if provided true value, icon will be rendered collapsed
- * state,  otherwise it will be rendered expanded state that includes attrribute not present.
+ * @param {Boolean} [is-collapsed=false] if provided true value, icon will be
+ * rendered collapsed state,  otherwise it will be rendered expanded state that
+ * includes is-collapsed attribute not present.
+ *
+ * @param {*} [not-collapsable] a attribute if presented, will hide the collapse / expand icon and
+ * making haeder un-clickable and in expanded state
+ *
  * @param {String} [header] String to use as the panel header
- * @param {Function} [onToggle] A callback function when user clicks expanded and collapsed icon
+ *
+ * @param {Function} [on-toggle] A callback function when user clicks expanded and collapsed icon
  *
  */
   .directive('akamContentPanel', require('./content-panel-directive'))
