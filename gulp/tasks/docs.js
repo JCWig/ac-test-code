1e6acf18108346702ae30a8cad6f12b059b857ed
Shawn Dahlen <shawn@dahlen.me>
2015-07-28 20:48:01 -0400

    Move doc generation directly into package

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -1,17 +1,51 @@
 'use strict';
 
 var gulp = require('gulp');
-var akamaiDocs = require('akamai-docs');
-var bundleLogger = require('../util/bundle-logger');
-var config = require('../config');
+var path = require('path');
+var Dgeni = require('dgeni');
+
+var pkg = new Dgeni.Package('akamai-package', [
+  require('dgeni-packages/ngdoc')
+])
+.config(function(dgeni, log, readFilesProcessor, writeFilesProcessor) {
+  dgeni.stopOnValidationError = true;
+  dgeni.stopOnProcessingError = true;
+
+  log.level = 'info';
+
+  readFilesProcessor.basePath = './src';
+  readFilesProcessor.sourceFiles = ['indeterminate-progress/**/*.js'];
+  writeFilesProcessor.outputFolder = '../docs';
+})
+.config(function(templateFinder) {
+  templateFinder.templateFolders.unshift(path.resolve(__dirname, 'templates'));
+})
+.config(function(computePathsProcessor) {
+    computePathsProcessor.pathTemplates.push({
+        docTypes: ['module'],
+        getPath: function(doc) {
+          return path.dirname(doc.fileInfo.relativePath);
+        },
+        outputPathTemplate: '${path}/index.html'
+    });
+})
+.processor(function filterDocsProcessor() {
+  return {
+    $runBefore: ['computePathsProcessor'],
+    $runAfter: ['providerDocsProcessor'],
+    $process: function(docs) {
+      return docs.filter(function(doc) {
+        return doc.docType === 'module';
+      });
+    }
+  };
+});
 
 gulp.task('docs', ['clean'], function() {
-  akamaiDocs({
-    sources: config.docs.sources,
-    base: config.docs.base,
-    output: config.docs.outputDirectory
-  })
-    .then(function(docs){
+    var dgeni = new Dgeni([pkg]);
+
+    dgeni.generate()
+    .then(function(docs) {
       console.log(docs.length, 'docs generated');
     })
     .catch(function(error) {
--- src/indeterminate-progress/index.js
+++ src/indeterminate-progress/index.js
@@ -7,7 +7,6 @@ const MODULE_NAME = 'akamai.components.indeterminate-progress';
  * @ngdoc module
  *
  * @name akamai.components.indeterminate-progress
- * @display Indeterminate Progress
  *
  * @description Provides a directive that creates Luna- and
  * Pulsar-compatible indeterminate progress elements. These
@@ -20,7 +19,8 @@ export default angular.module(MODULE_NAME, [])
 /**
  * @ngdoc directive
  *
- * @name akamai.components.indeterminate-progress.directive:akamIndeterminateProgress
+ * @name akamIndeterminateProgress
+ * @module akamai.components.indeterminate-progress
  *
  * @description Creates an indeterminate progress control
  *
