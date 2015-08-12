fdad44ee878872d060c2c7c525d7d954a8587d4c
Yair Leviel <yleviel@akamai.com>
2015-08-12 15:10:50 -0400

    FEE-1143 Allow the developer to run tests in a single folder by specifying: testDir=x where x is the directory name using the pattern: `spec/${x}/**/*.js`

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -5,9 +5,14 @@ var karma = require('karma');
 var path = require('path');
 var production = require('../config').productionBuild;
 
-gulp.task('test', ['lint', 'sync-locale'], function() {
+gulp.task('test', ['lint', 'sync-locale'], function(done) {
   karma.server.start({
     configFile: path.resolve('./karma.conf.js'),
-    singleRun: production
+    singleRun: true
+  },
+  // the karma run is always asynchronous,
+  // make sure to declare it's finished only when it executes the callback
+  function(exitCode){
+    done(exitCode);
   });
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,25 +1,33 @@
 'use strict';
 
 var istanbul = require('browserify-istanbul');
-var jenkins = !!require('yargs').argv.jenkins;
+var args = require('yargs').argv;
+var jenkins = !!args.jenkins;
 
 module.exports = function(config) {
+
+  var baseFileList = ['node_modules/angular/angular.js',
+    'node_modules/angular-mocks/angular-mocks.js',
+    'dist/akamai-core.min.css',
+    {pattern: 'dist/images/*', included: false, served: true},
+    'node_modules/moment/moment.js'];
+
+  var allSourceFiles = [
+    {pattern: 'spec/!(mega-menu)/**/*.js', watched: false},
+
+    // load the mega menu tests last because they seem to not clean up the environment
+    // properly and several message-box and modal-window tests end up failing
+    {pattern: 'spec/mega-menu/**/*.js', watched : false}
+  ];
+
+  var filesToTest = filesToTest = baseFileList.concat(
+    args.testDir ? [{pattern: 'spec/'+ args.testDir+'/**/*.js', watched: false}] : allSourceFiles
+  );
+
   config.set({
     colors: !jenkins,
     browserNoActivityTimeout: 60000,
-    files: [
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'dist/akamai-core.min.css',
-      {pattern: 'dist/images/*', included: false, served: true},
-      'node_modules/moment/moment.js',
-
-      'spec/!(mega-menu)/**/*.js',
-
-      // load the mega menu tests last because they seem to not clean up the environment
-      // properly and several message-box and modal-window tests end up failing
-      'spec/mega-menu/**/*.js'
-    ],
+    files: filesToTest,
     logLevel: config.LOG_ERROR,
     frameworks: ['browserify', 'jasmine', 'jquery-2.1.0', 'sinon'],
     preprocessors: {
