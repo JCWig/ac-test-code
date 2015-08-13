e7b6945081089283701cf78413c435408c8a0d4c
sewang <sewang@akamai.com>
2015-08-13 15:16:27 -0400

    FEE-1097 FEE-1013 implemented ES6 for status message

--- src/status-message/status-message-directive.js
+++ src/status-message/status-message-directive.js
@@ -1,13 +1,70 @@
 import template from './templates/status-message-directive.tpl.html';
 
-class StatusMessageController {
-  constructor() {
+const DEFAULT_TIMEOUT = 2000;
+const [SUCCESS] = ['success'];
 
+class StatusMessageController {
+  constructor($log, $timeout) {
+    this.$log = $log;
+    this.$timeout = $timeout;
+
+    this.timeout = null;
+    this.closing = false;
+
+    if (!this.status) {
+      this.status = SUCCESS;
+    }
   }
 }
 
-function linkFn() {
+StatusMessageController.$inject = ['$log', '$timeout'];
 
+function linkFn(scope, element, attrs) {
+  let timer = null,
+    ctrl = scope.statusMessage;
+
+  element.addClass(ctrl.status);
+
+  ctrl.timeout = !attrs.timeout ? DEFAULT_TIMEOUT : window.parseInt(attrs.timeout, 10);
+  if (isNaN(ctrl.timeout) || ctrl.timeout < 0) {
+    ctrl.timeout = DEFAULT_TIMEOUT;
+  }
+
+  scope.close = function() {
+    //make sure we're not allowing the callback to (also) occur if the user clicked close
+    cancelTimer();
+    ctrl.closing = true;
+    ctrl.$timeout(() => {
+      element.remove();
+      ctrl.closing = false;
+    }, 500);
+  };
+
+  element.on('$destroy', () => {
+    scope.$emit('akam-status-message-destroyed', ctrl.itemId);
+    element.off('mouseenter', cancelTimer);
+    element.off('mouseleave', enableTimer);
+  });
+
+  function enableTimer() {
+    if (ctrl.timeout > 0) {
+      timer = ctrl.$timeout(() => {
+        scope.close();
+      }, ctrl.timeout);
+    }
+  }
+
+  function cancelTimer() {
+    if (timer != null) {
+      ctrl.$timeout.cancel(timer);
+      timer = null;
+    }
+  }
+
+  element.on('mouseenter', cancelTimer);
+  element.on('mouseleave', enableTimer);
+
+  enableTimer();
 }
 
 export default () => {
@@ -16,7 +73,7 @@ export default () => {
     scope: {},
     replace: true,
     template: template,
-    linkFn,
+    link: linkFn,
     controller: StatusMessageController,
     controllerAs: 'statusMessage',
     bindToController: {
@@ -26,68 +83,3 @@ export default () => {
     }
   };
 };
-
-
-module.exports = function($log, $timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      itemId: '@',
-      text: '@',
-      status: '@'
-    },
-    replace: true,
-    template: template,
-    link: function(scope, element, attrs) {
-      var defaultTimeout = 2000;
-      var timer = null;
-
-      if (scope.status == null || scope.status === '') {
-        scope.status = 'success';
-      }
-
-      element.addClass(scope.status);
-
-      scope.timeout = attrs.timeout == null ? defaultTimeout : window.parseInt(attrs.timeout, 10);
-      if (isNaN(scope.timeout) || scope.timeout < 0) {
-        scope.timeout = defaultTimeout;
-      }
-
-      scope.close = function() {
-        //make sure we're not allowing the callback to (also) occur if the user clicked close
-        cancelTimer();
-        scope.closing = true;
-        $timeout(function() {
-          element.remove();
-        }, 500);
-      };
-
-      element.on('$destroy', function() {
-        scope.$emit('akam-status-message-destroyed', scope.itemId);
-        element.off('mouseenter', cancelTimer);
-        element.off('mouseleave', enableTimer);
-      });
-
-      function enableTimer() {
-        if (scope.timeout > 0) {
-          timer = $timeout(function() {
-            scope.close();
-          }, scope.timeout);
-        }
-      }
-
-      function cancelTimer() {
-        if (timer != null) {
-          $timeout.cancel(timer);
-          timer = null;
-        }
-      }
-
-      element.on('mouseenter', cancelTimer);
-      element.on('mouseleave', enableTimer);
-
-      enableTimer();
-    }
-  };
-};
-module.exports.$inject = ['$log', '$timeout'];
