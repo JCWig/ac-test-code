9d68c77344ca24cabf7f321c3090d7342cad0373
Yair Leviel <yleviel@akamai.com>
2015-08-27 15:04:22 -0400

    FEE-1165 Updated spinner controller to be a fat arrow returning an object. Make the spinner stay in the vertical and horizontal center.

--- src/spinner-button/spinner-button-directive.js
+++ src/spinner-button/spinner-button-directive.js
@@ -1,12 +1,10 @@
 import template from './templates/spinner-button.tpl.html';
 
-class SpinnerButtonController {}
-
 export default () => {
   return {
     restrict: 'E',
     template: template,
-    controller: SpinnerButtonController,
+    controller: () => {},
     controllerAs: 'spinnerButton',
     bindToController: {
       text: '@?',
