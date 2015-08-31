972465de12c2c49699e94dd568b539bd035297e4
Yair Leviel <yleviel@akamai.com>
2015-08-31 18:09:47 -0400

    FEE-1249 Added/Updated images for spinner according to new UXD guidance.  Added new micro size. Added example for new state.

--- src/indeterminate-progress/indeterminate-progress-directive.js
+++ src/indeterminate-progress/indeterminate-progress-directive.js
@@ -3,24 +3,19 @@ import template from './templates/indeterminate-progress-directive.tpl.html';
 
 const WRAPPER_CLASS_NAME = 'indeterminate-progress-wrapper',
   CLASS_NAME = 'indeterminate-progress',
+  SIZE_MICRO = 'micro',
   SIZE_SMALL = 'small',
   SIZE_NORMAL = 'normal',
   SIZE_LARGE = 'large',
+  SIZES = [SIZE_LARGE, SIZE_NORMAL, SIZE_SMALL, SIZE_MICRO],
   STATE_STARTED = 'started',
   STATE_COMPLETED = 'completed',
-  STATE_FAILED = 'failed';
+  STATE_FAILED = 'failed',
+  STATE_SUCCESS = 'success';
 
 class IndeterminateProgressController {
-
   get size() {
-    switch (this.spinnerSize) {
-      case SIZE_SMALL:
-        return SIZE_SMALL;
-      case SIZE_LARGE:
-        return SIZE_LARGE;
-      default:
-        return SIZE_NORMAL;
-    }
+    return SIZES.indexOf(this.spinnerSize) > -1 ? this.spinnerSize : SIZE_NORMAL;
   }
 
   get state() {
@@ -28,11 +23,19 @@ class IndeterminateProgressController {
       return STATE_FAILED;
     }
 
+    if (this.success) {
+      return STATE_SUCCESS;
+    }
+
     if (this.completed) {
       return STATE_COMPLETED;
     }
 
     return STATE_STARTED;
+  }
+
+  get success() {
+    return this.stateSuccess === 'true';
   }
 
   get completed() {
@@ -53,21 +56,25 @@ function indeterminateProgressDirective(translate) {
       label: '@label',
       stateFailed: '@failed',
       stateCompleted: '@completed',
+      stateSuccess: '@success',
       spinnerSize: '@size'
     },
     controller: IndeterminateProgressController,
     controllerAs: 'indeterminateProgress',
-    link: (scope, element) => {
-      let ctrl = scope.indeterminateProgress;
-
+    link: (scope, element, attrs, ctrl) => {
       if (angular.isDefined(ctrl.label) && ctrl.label !== '') {
         translate.async(ctrl.label)
           .then(value => ctrl.label = value);
       }
 
-      //add or remove the class based on whether or not the element is "completed".
+      //add or remove the class based on whether or not the element is "failed".
       scope.$watch('indeterminateProgress.stateFailed', () => {
         element.toggleClass('failed', ctrl.failed);
+      }, true);
+
+      //add or remove the class based on whether or not the element is "success"
+      scope.$watch('indeterminateProgress.stateSuccess', () => {
+        element.toggleClass('success', ctrl.success);
       }, true);
 
       //add or remove the class based on whether or not the element is "completed".
