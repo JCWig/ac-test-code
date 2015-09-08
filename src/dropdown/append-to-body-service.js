43c8b2bea4ea9df0c1e767888b3db86188215875
Miguel Bollar <mbollar@akamai.com>
2015-09-08 14:31:18 -0400

    FEE-1162: fixed dropdown-menu positioning when appended-to-body

--- src/dropdown/append-to-body-service.js
+++ src/dropdown/append-to-body-service.js
@@ -11,25 +11,25 @@ export default class appendToBodyService {
     this.$document = $document;
   }
 
-  setAppendToBodyCoords() {
-    let menu = this.elem.children(0)[0];
+  setAppendToBodyCoords(elem, menuElem) {
+    let menu = elem.children(0)[0];
 
-    this.menuElem.css({
+    menuElem.css({
       left: menu.offsetLeft + 'px',
       top: menu.offsetTop + menu.offsetHeight + 'px'
     });
   }
 
   appendToBody(elem, menuElem, callBack) {
-    this.elem = elem;
-    this.menuElem = menuElem;
     this.$timeout(() => {
       menuElem.addClass('append-body util-hide');
-      this.setAppendToBodyCoords();
+      this.setAppendToBodyCoords(elem, menuElem);
       angular.element(this.$document.find('body')).append(menuElem);
       let windowElement = angular.element(window);
 
-      windowElement.on('resize', debounce(this.setAppendToBodyCoords, 200));
+      windowElement.on('resize', debounce(() => {
+        this.setAppendToBodyCoords(elem, menuElem);
+      }, 200));
       elem.on('$destroy', function() {
         windowElement.off('resize');
       });
