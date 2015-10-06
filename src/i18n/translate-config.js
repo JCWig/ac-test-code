203e4dd41960d64ba19cf39ef4d72319edffb667
Miguel Bollar <mbollar@akamai.com>
2015-10-06 11:13:44 -0400

    FEE-1373: Created i18nLocaleProvider and i18nLocale service. i18nLocale will be set externally (e.g. akamai-portal). FEE-1373: Removed previous run block approach to setting properties on $locale directly. FEE-1373: Re-implemented previous decorator/config approach which now uses i18nLocale.

--- src/i18n/translate-config.js
+++ src/i18n/translate-config.js
@@ -1,3 +1,5 @@
+import LOCALES from './i18n-locale-constant';
+
 export const i18nConfig = {
   defaultLocale: 'en_US',
   availableLangKeys: ['de_DE', 'en_US', 'en_US_ATT', 'es_ES', 'es_LA', 'fr_FR', 'it_IT',
@@ -33,6 +35,42 @@ function config($provide, $translateProvider) {
     .preferredLanguage(i18nConfig.defaultLocale)
     .fallbackLanguage(i18nConfig.defaultLocale)
     .cloakClassName('util-hide');
+
+  /**
+   * a decorator to intercept $locale service and add datetime and number
+   * values specific for current locale
+   * @param  {object} $delegate original $locale service object
+   * @param  {object} i18nToken a factory service holds value of current locale
+   * @param  {object} LOCALES locale constant
+   * @return {object} $delegate modified $locale service object
+   */
+  $provide.decorator('$locale', ['$delegate', 'i18nLocale',
+    ($delegate, i18nLocale) => {
+
+      Object.defineProperty($delegate, 'DATETIME_FORMATS', {
+        get: () => {
+          let loc = i18nLocale ?
+            LOCALES[i18nLocale] : LOCALES[i18nConfig.defaultLocale];
+
+          if (loc) {
+            return loc.DATETIME_FORMATS;
+          }
+        }
+      });
+
+      Object.defineProperty($delegate, 'NUMBER_FORMATS', {
+        get: () => {
+          let loc = i18nLocale ?
+            LOCALES[i18nLocale] : LOCALES[i18nConfig.defaultLocale];
+
+          if (loc) {
+            return loc.NUMBER_FORMATS;
+          }
+        }
+      });
+
+      return $delegate;
+    }]);
 }
 config.$inject = ['$provide', '$translateProvider'];
 
