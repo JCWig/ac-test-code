8d98953e0bf10c76cec150911d2192d78b4ae89e
Miguel Bollar <mbollar@akamai.com>
2015-10-07 11:22:17 -0400

    FEE-1373: fixed translations in example pages

--- examples/tabs/routing/routing-tabs.js
+++ examples/tabs/routing/routing-tabs.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('navRoutingApp', ['akamai.components.tabs'])
+angular.module('navRoutingApp', ['akamai.components.constants', 'akamai.components.tabs'])
   .config(configFunction);
 
-function configFunction($stateProvider, $urlRouterProvider) {
+function configFunction($stateProvider, $urlRouterProvider, $translatePartialLoaderProvider,
+                        VERSION, $translateProvider, i18nLocaleProvider) {
 
   $urlRouterProvider
     .when('/', '/activity')
@@ -37,6 +38,20 @@ function configFunction($stateProvider, $urlRouterProvider) {
     templateUrl: 'health/sleep.html'
   });
 
+  // need to overwrite locales path on fee.akamai.com
+  if (window.location.host == 'fee.akamai.com') {
+    $translatePartialLoaderProvider.addPart('../dist/locales/');
+    $translatePartialLoaderProvider.addPart('locales/json/messages/');
+  } else {
+    $translatePartialLoaderProvider.addPart('/libs/akamai-core/'+VERSION+'/locales/');
+    $translatePartialLoaderProvider.addPart('/apps/akamai-core-examples/locales/');
+  }
+
+  var locale = docCookies.getItem('AKALOCALE') || 'en_US';
+
+  $translateProvider.preferredLanguage(locale);
+  i18nLocaleProvider.setLocale(locale);
 }
 
-configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
+configFunction.$inject = ['$stateProvider', '$urlRouterProvider', '$translatePartialLoaderProvider',
+  'AKAMAI_CORE_VERSION', '$translateProvider', 'i18nLocaleProvider'];
--- examples/tabs/static/static-tabs.js
+++ examples/tabs/static/static-tabs.js
@@ -1,6 +1,27 @@
-angular.module('navApp', ['akamai.components.tabs'])
-  .controller('NavAppController', NavAppController);
+angular.module('navApp', ['akamai.components.constants', 'akamai.components.tabs'])
+  .controller('NavAppController', NavAppController)
+  .config(configFunction);
 
 function NavAppController() {
   this.foo = 'bar';
 }
+
+function configFunction($translatePartialLoaderProvider, VERSION, $translateProvider,
+                        i18nLocaleProvider) {
+
+  // need to overwrite locales path on fee.akamai.com
+  if (window.location.host == 'fee.akamai.com') {
+    $translatePartialLoaderProvider.addPart('../dist/locales/');
+    $translatePartialLoaderProvider.addPart('locales/json/messages/');
+  } else {
+    $translatePartialLoaderProvider.addPart('/libs/akamai-core/'+VERSION+'/locales/');
+    $translatePartialLoaderProvider.addPart('/apps/akamai-core-examples/locales/');
+  }
+
+  var locale = docCookies.getItem('AKALOCALE') || 'en_US';
+
+  $translateProvider.preferredLanguage(locale);
+  i18nLocaleProvider.setLocale(locale);
+}
+configFunction.$inject = ['$translatePartialLoaderProvider', 'AKAMAI_CORE_VERSION',
+  '$translateProvider', 'i18nLocaleProvider'];
