f072bf8141faadf9035d8434e3b885127cb9b45d
Miguel Bollar <mbollar@akamai.com>
2015-11-18 16:16:25 -0500

    FEE-1154, FEE-1669: Removed documentation for deprecated controls

--- src/set-focus/index.js
+++ src/set-focus/index.js
@@ -1,40 +1,6 @@
 import angular from 'angular';
 import setFocusDirective from './set-focus-directive';
 
-/**
- * @ngdoc module
- * @name akamai.components.set-focus
- *
- * @description
- * A utility directive that enables a developer to programmatically set focus on a
- * given element.
- */
 export default angular.module('akamai.components.set-focus', [])
-
-  /**
-   * @ngdoc directive
-   * @name akamSetFocus
-   * @description Provides a directive to programmatically set focus on a given
-   * element.
-   *
-   * **General Usage**
-   *
-   * The directive accepts a bound boolean value. When set to true, focus is set
-   * and when set to false the element is blurred.
-   *
-   * ```
-   * class MyController {
-   *   constructor() {
-   *     this.focusEnabled = true;
-   *   }
-   *   removeFocus() {
-   *     this.focusEnabled = false;
-   *   }
-   * }
-   * ```
-   *
-   * ```
-   * <div akam-set-focus="focusEnabled"></div>
-   * ```
-   */
+  
   .directive('akamSetFocus', setFocusDirective);
