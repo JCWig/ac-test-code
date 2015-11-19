fae7fd032f010d33c2ae53077dd57da75ada2ff6
Miguel Bollar <mbollar@akamai.com>
2015-11-19 09:58:02 -0500

    FEE-1154, FEE-1669: Removed documentation for deprecated controls
    - fixed eslint error

--- src/set-focus/index.js
+++ src/set-focus/index.js
@@ -2,5 +2,5 @@ import angular from 'angular';
 import setFocusDirective from './set-focus-directive';
 
 export default angular.module('akamai.components.set-focus', [])
-  
+
   .directive('akamSetFocus', setFocusDirective);
